1482397658188b64a5269e49af19b15cf30ae33f
Darcio Vilela <darcio@jp7.com.br>
2021-11-26 11:10:34 -0300

    Adjustments in sendMail function and transporter using nodemailer

--- src/mail.ts
+++ src/mail.ts
@@ -3,20 +3,32 @@ import nodemailer from 'nodemailer';
 
 dotenv.config();
 
-export const sendMail = async (to: string) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: parseInt(process.env.SMTP_PORT),
+interface SendMailOptions {
+  to: string;
+  subject: string;
+  message: string;
+}
+export const sendMail = async (options: SendMailOptions) => {
+  const transportOptions = {
+    host: process.env.SMTP_HOST as string,
+    port: parseInt(process.env.SMTP_PORT as string),
     secure: process.env.SMTP_SECURE === 'true',
     auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
+      user: process.env.SMTP_USER as string,
+      pass: process.env.SMTP_PASS as string,
     },
-  });
+  };
+  const transporter = nodemailer.createTransport(transportOptions);
+
+  const { to, subject, message } = options;
 
   const info = await transporter.sendMail({
-    from: `"${process.env.EMAIL_FROM_NAME}" <${process.env.EMAIL_FROM}>`,
+    from: `${process.env.EMAIL_FROM_NAME}<${process.env.EMAIL_FROM}>`,
     to,
-    subject: 'Hello ✔',
-    html: '<b>Hello world?</b>',
+    subject,
+    // message,
+    html: message,
   });
+
+  console.log(info);
+};
